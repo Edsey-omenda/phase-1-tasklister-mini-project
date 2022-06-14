e0840980f90fef8521f41ffd0d0ea2cac2c128c1
Edsey omenda <omendajames0317@gmail.com>
2022-06-14 21:05:15 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", (e)=>{
+    e.preventDefault()
+    createToDo(document.getElementById("new-task-description").value)
+  })
 });
+
+function createToDo(todo){
+  let p = document.createElement("p");
+  let btn = document.createElement("button")
+  btn.addEventListener("click", takeOut)
+  btn.textContent = "del"
+  p.textContent = `${todo}`
+  p.appendChild(btn)
+  document.querySelector("#list").appendChild(p)
+}
+function takeOut(){
+  let element = document. getElementById("list");
+   element. parentNode. removeChild(element);
+}
